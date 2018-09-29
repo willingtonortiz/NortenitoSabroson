0b5cb9c9c402efd7dc8c21cb9d7591dd0d928089
Willington <willingtonortizmaurtua@hotmail.com>
2018-09-29 16:37:03 -0500

    Se agregó git a NetBeans

---
+++ Nortenito/src/nortenito/Nortenito.java
@@ -0,0 +1,12 @@
+package nortenito;
+
+public class Nortenito {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo Norteñito");
+        
+    }
+    
+}
