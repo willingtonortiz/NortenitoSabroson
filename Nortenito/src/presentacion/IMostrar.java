236ff99af2cc7d94f67dd1209f755674b818107e
Willington <willingtonortizmaurtua@hotmail.com>
2018-11-16 20:18:42 -0500

    Se borraron las vistas por consola porque se utilizará interfaz gráfica

--- Nortenito/src/presentacion/IMostrar.java
+++
@@ -1,7 +0,0 @@
-package presentacion;
-
-public interface IMostrar {
-    
-    void mostrar();
-    
-}
--- Nortenito/src/presentacion/Main.java
+++ Nortenito/src/presentacion/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        VistaMenu vista = new VistaMenu();
         
-        vista.mostrar();
         
     }    
 }
